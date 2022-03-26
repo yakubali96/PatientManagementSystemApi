659d933bd26ec1bdca7732c21e69769ef3fb1db0
yakub ali <ali1263399@gmail.com>
2022-03-26 21:26:06 +0600

    first commit

---
+++ PatientManagementSystem/src/main/java/com/example/PatientManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/model/Information.java
@@ -0,0 +1,57 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "information")
+public class Information {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int patientId;
+	private String FamilyMemberName;
+	private String relationship;
+	private int phoneNo;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+	public String getFamilyMemberName() {
+		return FamilyMemberName;
+	}
+	public void setFamilyMemberName(String familyMemberName) {
+		FamilyMemberName = familyMemberName;
+	}
+	public String getRelationship() {
+		return relationship;
+	}
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
